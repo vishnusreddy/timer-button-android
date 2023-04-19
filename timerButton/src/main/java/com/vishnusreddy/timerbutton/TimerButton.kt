69eebdb1d0c96912f769cdd93095a96c9a3340cd
Vishnu S Reddy <vishnu.sr@shadowfax.in>
2023-04-19 23:15:33 +0530

    Added README.md.

--- timerButton/src/main/java/com/vishnusreddy/timerbutton/TimerButton.kt
+++ timerButton/src/main/java/com/vishnusreddy/timerbutton/TimerButton.kt
@@ -2,10 +2,20 @@ package com.vishnusreddy.timerbutton
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.vishnusreddy.timerbutton.databinding.TimerButtonBinding
 
 class TimerButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : ConstraintLayout(context, attrs) {
-    
+    var binding: TimerButtonBinding
+    init {
+        binding = TimerButtonBinding.inflate(LayoutInflater.from(context))
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.cta
+    }
 }
